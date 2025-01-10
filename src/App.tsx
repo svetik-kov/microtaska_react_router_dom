dc2d3caeaf240c8f12d5ca476afe1226f32d1441
Svetlana_Kovalevich <svetik14-06@mail.ru>
2025-01-10 13:35:47 +0300

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,46 @@
-import React from "react"
-import styles from "./components/Site.module.css"
-import { PageOne } from "components/pages/PageOne"
-import { PageTwo } from "components/pages/PageTwo"
-import { PageThree } from "components/pages/PageThree"
-import { Navigate, NavLink, Route, Routes } from "react-router-dom"
-import { Error404 } from "components/pages/Error404"
-import { S } from "components/pages/_styles"
+import React from 'react';
+import {Navigate, NavLink, Route, Routes} from 'react-router-dom';
+import {Error404} from "./components/pages/Error404";
+import {PageOne} from "./components/pages/PageOne";
+import {PageTwo} from "./components/pages/PageTwo";
+import {PageThree} from "./components/pages/PageThree";
+import styles from "./components/Site.module.css";
+import {S} from './components/pages/_styles';
 
-const PATH={
-  PAGE1:'/adidas',
-  PAGE2:'/puma',
-  PAGE3:'/abibas',
-  ERROR404:'/error404'
-} as const
+
+const PATH = {
+    PAGE1: '/page1',
+    PAGE2: '/page2',
+    PAGE3: '/page3',
+} as const;
+
 function App() {
-  return (
-    <div>
-      <div className={styles.header}><h1>HEADER</h1></div>
-      <div className={styles.body}>
-        <div className={styles.nav}>
-
-
-          {/*      <div>
-                  <NavLink to="/adidas"
-                           className={({ isActive }) => isActive ? styles.activeNavLink : styles.navLink}>
-                    Adidas
-                  </NavLink>
+    return (
+        <div>
+            <div className={styles.header}><h1>HEADER</h1></div>
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    <S.NavWrapper><NavLink to={PATH.PAGE1}>Page1</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE2}>Page2</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE3}>Page3</NavLink></S.NavWrapper>
+                    <a href="page3">page3 HTML</a>
                 </div>
-                <div>
-                  <NavLink to="/puma"
-                           className={({ isActive }) => isActive ? styles.activeNavLink : styles.navLink}>
-                    Puma
-                  </NavLink>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path={'/'} element={<Navigate to={'/page1'}/>}/>
+
+                        <Route path={PATH.PAGE1} element={<PageOne/>}/>
+                        <Route path={PATH.PAGE2} element={<PageTwo/>}/>
+                        <Route path={PATH.PAGE3} element={<PageThree/>}/>
+
+                        <Route path={'/*'} element={<Error404/>}/>
+                    </Routes>
                 </div>
-                <div>
-                  <NavLink to="/abibas"
-                           className={({ isActive }) => isActive ? styles.activeNavLink : styles.navLink}>
-                    Abibas
-                  </NavLink>
-                </div> */}
-          <S.NavWrapper><NavLink to={PATH.PAGE1}> Adidas</NavLink></S.NavWrapper>
-          <S.NavWrapper><NavLink to={PATH.PAGE2}>Puma</NavLink></S.NavWrapper>
-          <S.NavWrapper><NavLink to={PATH.PAGE3}>Abibas</NavLink></S.NavWrapper>
+            </div>
+            <div className={styles.footer}>abibas 2023</div>
         </div>
-        <div className={styles.content}>
-
-          <Routes>
-            <Route path={PATH.PAGE1} element={<PageOne />} />
-            <Route path={PATH.PAGE2} element={<PageTwo />} />
-            <Route path={PATH.PAGE3} element={<PageThree />} />
-            <Route path={PATH.ERROR404} element={<Error404 />} />
-            <Route path="/*" element={<Navigate to={PATH.ERROR404} />} />
-            <Route path="/" element={<Navigate to={PATH.PAGE1} />} />
-
-          </Routes>
-          {/* <PageOne/>
-                  <PageTwo/>
-                  <PageThree/>*/}
-        </div>
-      </div>
-      <div className={styles.footer}>abibas 2023</div>
-    </div>
-  )
+    );
 }
 
-export default App
-
-
-
+export default App;
 
