e496a62e2e1c07fbac4d1e125cf554564bfe4ed4
Svetlana_Kovalevich <svetik14-06@mail.ru>
2025-01-12 22:57:20 +0300

    useParams + find

--- src/components/pages/Model.tsx
+++ src/components/pages/Model.tsx
@@ -2,23 +2,31 @@ import React from "react"
 import { useParams } from "react-router-dom"
 import { adidasArr } from "components/pages/Adidas"
 import { pumaArr } from "components/pages/Puma"
+import { Error404 } from "components/pages/Error404"
 
 export const Model = () => {
   const params = useParams()
   const { id } = params
   console.log(id)
 
-  const array=[...adidasArr,...pumaArr]
+  const array = [...adidasArr, ...pumaArr]
+
+  const currentModel=array.find(el=>el.id===Number(id))
   return (
     <div>
-      MODEL: <h3 style={{ color: "steelblue" }}>{array[Number(id)] && array[Number(id)].model}</h3>
-      <div>price:{array[Number(id)] && array[Number(id)].price}  </div>
-      <div>< img
-        src={array[Number(id)].picture}
-        alt={array[Number(id)].model}
-        style={{ width: "200px", height: "auto", marginRight: "10px" }}
-      /></div>
-
+      {currentModel ?
+        <>
+          MODEL: <h3 style={{ color: "steelblue" }}>{currentModel.model}</h3>
+          <div>price:{currentModel.price}  </div>
+          <div>
+            < img
+              src={currentModel.picture}
+              alt={currentModel.model}
+              style={{ width: "200px", height: "auto", marginRight: "10px" }}
+            />
+          </div>
+        </>
+        : <h2> Такой модели не существует</h2>}
 
     </div>
   )
